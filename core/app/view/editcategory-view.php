60c7e8d30cd8d06ae0c89920fccaf3605cce8baf
Ivan Picoy <picoy.ivan@gmail.com>
2019-09-04 16:40:23 -0300

    Modificacion nuevo prod y categ
    solo se realizo cambio de nombres

--- core/app/view/editcategory-view.php
+++ core/app/view/editcategory-view.php
@@ -1,7 +1,7 @@
 <?php $user = CategoryData::getById($_GET["id"]);?>
 <div class="row">
 	<div class="col-md-12">
-	<h1>Editar Categoria</h1>
+	<h1>Editar Marca</h1>
 	<br>
 		<form class="form-horizontal" method="post" id="addproduct" action="index.php?view=updatecategory" role="form">
 
@@ -16,7 +16,7 @@
   <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
     <input type="hidden" name="user_id" value="<?php echo $user->id;?>">
-      <button type="submit" class="btn btn-primary">Actualizar Categoria</button>
+      <button type="submit" class="btn btn-primary">Actualizar Marca</button>
     </div>
   </div>
 </form>
